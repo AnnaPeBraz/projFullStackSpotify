71e27ac059ee6547e7a1b0cae71b2495e086437b
unknown <annap.0103@gmail.com>
2025-02-13 15:43:03 -0300

    comeco das aulas complementares

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,27 @@
 // import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
   return (
-    <div className="main">Main</div>
-  )
-}
+    <div className="main">
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas Populares</h2>
+          <a className="item-list__link" href="">
+            Mostrar mais
+          </a>
+        </div>
 
-export default Main
+        <div className="single-item">
+          <img className="single-item__image" src="" alt="" />
+          <p>Nome do artista</p>
+          <p>Artista</p>
+          <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay} />{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
